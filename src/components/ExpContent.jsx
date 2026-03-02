828e2558c7055394072b407443de79137caea0fa
Ali <utfcystgvytf@gmail.com>
2026-03-02 16:11:49 +0200

    Add Hero Text & Layout

---
+++ src/components/ExpContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExpContent = () => {
+  return (
+    <div>ExpContent</div>
+  )
+}
+
+export default ExpContent
---
+++ src/sections/LogoShowcase.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LogoShowcase = () => {
+  return (
+    <div>LogoShowcase</div>
+  )
+}
+
+export default LogoShowcase
